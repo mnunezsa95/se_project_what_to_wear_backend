8d99b6a45f327cb02448eb141e4fae75cc540266
mnunezsa <mnunezsa95@gmail.com>
2023-09-04 17:13:48 -0400

    create user routes

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,7 @@
 const router = require("express").Router(); // import and create the router
+const { getCurrentUser, updateCurrentUser } = require("../controllers/users");
+
+router.get("/users/me", getCurrentUser);
+router.patch("/users/me", updateCurrentUser);
 
 module.exports = router;
