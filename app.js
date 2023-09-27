f0ff97888e82980a153868c13361a87ae2992535
mnunezsa <mnunezsa95@gmail.com>
2023-09-27 07:47:56 -0400

    update mongodb path

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { limiter } = require("./middlewares/rateLimiter");
 const { login, createUser } = require("./controllers/users");
 
 const app = express();
-mongoose.connect("mongodb://localhost:27017/wtwr_db"); // connect to mongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db"); // connect to mongoDB
 const { PORT = 3001 } = process.env; // set PORT default to 3001
 
 app.use(helmet());
